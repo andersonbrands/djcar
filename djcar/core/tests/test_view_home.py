afd55f52befb15d25c86f92c2adabfee01aac5ab
Anderson Brandao <anderson.brands@gmail.com>
2023-08-01 16:47:58 -0300

    Post formset in home view

--- djcar/core/tests/test_view_home.py
+++ djcar/core/tests/test_view_home.py
@@ -56,3 +56,50 @@ def test_no_cars_alert(home_resp, home_resp_with_car):
     text = "No cars registered yet, use the button above to add"
     assertContains(home_resp, text, 1)
     assertContains(home_resp_with_car, text, 0)
+
+
+def test_post_valid(client, valid_car_dict):
+    car = Car.objects.create(**valid_car_dict)
+    new_model = "Cool car"
+    url = reverse("home")
+
+    data = {
+        "form-TOTAL_FORMS": ["1"],
+        "form-INITIAL_FORMS": ["1"],
+        "form-MIN_NUM_FORMS": ["0"],
+        "form-MAX_NUM_FORMS": ["1000"],
+        "form-0-id": [car.id],
+        "form-0-model": [new_model],
+        "form-0-brand": [car.brand],
+        "form-0-main_color": [car.main_color],
+        "form-0-value": [car.value],
+        "form-0-production_cost": [car.production_cost],
+        "form-0-transportation_cost": [car.transportation_cost],
+    }
+    resp = client.post(url, data, follow=True)
+    assert not resp.context["formset"].errors
+    assertContains(resp, new_model)
+
+
+def test_post_invalid(client, valid_car_dict):
+    car = Car.objects.create(**valid_car_dict)
+    invalid_color = "bad color"
+
+    url = reverse("home")
+
+    data = {
+        "form-TOTAL_FORMS": ["1"],
+        "form-INITIAL_FORMS": ["1"],
+        "form-MIN_NUM_FORMS": ["0"],
+        "form-MAX_NUM_FORMS": ["1000"],
+        "form-0-id": [car.id],
+        "form-0-model": [car.model],
+        "form-0-brand": [car.brand],
+        "form-0-main_color": [invalid_color],
+        "form-0-value": [car.value],
+        "form-0-production_cost": [car.production_cost],
+        "form-0-transportation_cost": [car.transportation_cost],
+    }
+    resp = client.post(url, data, follow=True)
+    assert resp.context["formset"].errors
+    assertContains(resp, invalid_color)
--- djcar/core/views.py
+++ djcar/core/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import FormView
@@ -10,10 +10,19 @@ from djcar.core.forms import CarForm, CarFormSet
 # Create your views here.
 class Home(View):
     formset_class = CarFormSet
+    template_name = "core/index.html"
 
     def get(self, request):
         context = {"formset": self.formset_class()}
-        return render(request, "core/index.html", context)
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        formset = self.formset_class(request.POST)
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect(".")
+
+        return render(request, self.template_name, {"formset": formset})
 
 
 class AddCarView(FormView):
