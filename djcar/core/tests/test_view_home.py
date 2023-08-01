16306adb4255d4e4fe126ebe60d9263f5733f0f4
Anderson Brandao <anderson.brands@gmail.com>
2023-08-01 13:10:04 -0300

    Update templates

--- djcar/core/tests/test_view_home.py
+++ djcar/core/tests/test_view_home.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertContains, assertTemplateUsed
 
 
 @pytest.fixture
@@ -14,4 +14,22 @@ def test_get(home_resp):
 
 
 def test_template(home_resp):
+    assertTemplateUsed(home_resp, "core/base.html")
     assertTemplateUsed(home_resp, "core/index.html")
+
+
+@pytest.mark.parametrize(
+    "expected",
+    [
+        # bootstrap
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
+        # jquery
+        "https://code.jquery.com/jquery-3.7.0.min.js",
+        # main css and js
+        "css/main.css",
+        "js/main.js",
+    ],
+)
+def test_contains(home_resp, expected):
+    assertContains(home_resp, expected)
